43aeba2a43825682b9c5933be2d44fdcbcfecc9e
hatto <ryosuke.10117@gmail.com>
2023-02-18 13:56:28 +0900

    recostruction setting

---
+++ practice/test/test.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// clang-format off
+typedef long long ll;
+template <class T>
+using V = std::vector<T>;
+template <class T, class E>
+using P = std::pair<T, E>;
+#define rep(i, n, m) for (int i = n; i < (int)(m); ++i)
+#define rrep(i, n, m) for (int i = n - 1; i >= (int)(m); --i)
+#define fore(i, a) for (auto &i : a)
+#define veccin(v) \
+    for (auto &e : (v)) cin >> e
+#define veccout(v) \
+    for (auto &e : (v)) cout << e
+#define chmax(a, b) ((a) < (b) ? (a = b, true) : false)
+#define chmin(a, b) ((a) > (b) ? (a = b, true) : false)
+#define INF 1e9
+#define MOD 998244353
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a;
+
+    return 0;
+}
