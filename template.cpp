a3a91eb277e1c92177fcbea01e4678fc08d5c89f
hatto <ryosuke.10117@gmail.com>
2023-05-28 20:56:42 +0900

    add YesNo macro

--- template.cpp
+++ template.cpp
@@ -10,10 +10,12 @@ template <class T, class E> using P = std::pair<T, E>;
 #define rrep(i, n, m) for (int i = n - 1; i >= (int)(m); --i)
 #define fore(i, a) for (auto &i : a)
 #define veccin(x) for (auto &i : x) std::cin >> i                                                          
-#define veccout(x) for (auto &i : x) std::cout << i << " "
+#define veccout(x) for (auto &i : x) std::cout << i << " "; cout << endl;
 #define all(v) v.begin(), v.end()
 #define chmax(a, b) ((a) < (b) ? (a = b, true) : false)
 #define chmin(a, b) ((a) > (b) ? (a = b, true) : false)
+#define Yes(b) cout << ((b) ? "Yes" : "No") << endl;
+#define YES(b) cout << ((b) ? "YES" : "NO") << endl;
 #define INF 1e9
 #define LINF 1e18
 #define MOD 998244353
